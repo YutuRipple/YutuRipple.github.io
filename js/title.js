165562e1e273cd48798cc00e13371e225f8211d5
YutuRipple <ericripple617@gmail.com>
2023-09-03 21:35:11 +0800

    Site updated: 2023-09-03 21:35:10

--- js/title.js
+++ js/title.js
@@ -1,1 +1,17 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="😦别走啊，肥来看看~",clearTimeout(titleTime)):(document.title="😋嗨呀，就知道你舍不得～",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '😦别走啊，肥来看看~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '😋嗨呀，就知道你舍不得～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
